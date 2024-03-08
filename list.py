47589f1eb14b243962059e8c9b629782e2b02408
MukashfaTulAin <162397522+MukashfaTulAin@users.noreply.github.com>
2024-03-07 18:02:49 -0800

    Add files via upload
    I build project on python

---
+++ list.py
@@ -0,0 +1,17 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[1:4])
+
+thisdict = {
+  "brand": "stylo",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
